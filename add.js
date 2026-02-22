7e1f042704741108c96554476ca52c1759e4493f
sardor valiev <sardorvaliev005@gmail.com>
2026-02-22 13:21:49 +0500

    first commit

---
+++ add.js
@@ -0,0 +1,189 @@
+// ======================================================================
+
+// 1) Ikki sonni qo‘shish
+// Foydalanuvchidan 2 ta son olib, yig‘indisini chiqaring
+
+// console.log(name, surname, 'age - ', age, ' address - ',address);
+
+// ===================================================================================
+
+// let num1 = +prompt("Enter num1");
+// let num2 = +prompt("Enter num2");
+
+// let res = num1 + num2;
+// console.log("Natija - ", res);
+// ===================================================================================
+
+// 2) Ikki sonni ayirish
+// Birinchi sondan ikkinchi sonni ayiring va natijani chiqaring.
+
+// let num1 = +prompt("enter num1: ");
+// let num2 = +prompt("enter num2: ");
+
+// let res = num1 - num2;
+// console.log(res);
+// =====================================================================
+
+// 3) Ko‘paytirish
+// 2 ta sonni ko‘paytiring va natijani chiqaring.
+
+// let num1 = +prompt("enter num1: ");
+// let num2 = +prompt("enter num2: ");
+
+// let res = num1 * num2;
+// console.log(res);
+
+// ==============================================================
+
+// 4) Bo‘lish
+// Bir sonni ikkinchi songa bo‘ling
+
+// let num1 = +prompt("enter num1: ");
+// let num2 = +prompt("enter num2: ");
+
+// let res = num1 / num2;
+// console.log(res);
+
+// =============================================================
+
+// 5) To‘rt amal birga
+// 2 ta son uchun qo‘shish, ayirish, ko‘paytirish va bo‘lishni bajaring.
+
+// let num1 = +prompt("enter num1: ");
+// let num2 = +prompt("enter num2: ");
+
+// let res1 = num1 + num2;
+// let res2 = num1 - num2;
+// let res3 = num1 * num2;
+// let res4 = num1 / num2;
+
+// console.log(num1, ` + `, num2, ` = `, res1);
+// console.log(num1, ` - `, num2, ` = `, res2);
+// console.log(num1, ` * `, num2, ` = `, res3);
+// console.log(num1, ` / `, num2, ` = `, res4);
+
+// ==================================================================
+
+// 6) Kvadratini topish
+// Bitta sonning kvadratini toping.
+
+// let num1 = +prompt("enter num: ");
+
+// let res = num1 * num1;
+// console.log(num1, ` => `, res);
+
+// ============================================================
+
+// 7) Kubini topish
+// Bitta sonning kubini toping.
+
+// let num1 = +prompt("enter num: ");
+
+// let res = num1 ** num1;
+// console.log(num1, ` => `, res);
+
+// ==========================================================
+
+// 8) Kvadrat perimetri
+// Kvadrat tomoni berilgan. Perimetrini toping.
+
+// let num1 = +prompt("enter side: ");
+
+// let res = 4 * num1;
+// console.log("P - ", res);
+// ==========================================================
+
+// 9) Kvadrat yuzasi
+// Kvadrat yuzasini toping. Formula: S = a * a
+
+// let num1 = +prompt("enter side: ");
+
+// let res = num1 * num1;
+// console.log("S - ", res);
+// ============================================================
+
+// 11) To‘g‘ri to‘rtburchak yuzasi
+// a va b berilgan. Yuzasini toping.
+
+// let num1 = +prompt("enter side1: ");
+// let num2 = +prompt("enter side2: ");
+
+// let res = num1 * num2;
+// console.log("S - ", res);
+// ============================================================
+
+// 13) Vaqtni topish
+// Masofa va tezlik berilgan. Vaqtni toping.
+
+// let num1 = +prompt("enter km: ");
+// let num2 = +prompt("enter speed: ");
+
+// let res = num1 / num2;
+// console.log("t - ", res);
+// ============================================================
+
+// 14) Masofani topish
+// Tezlik va vaqt berilgan. Masofani toping.
+
+// let num1 = +prompt("enter km: ");
+// let num2 = +prompt("enter time: ");
+
+// let res = num1 * num2;
+// console.log("S - ", res);
+// ============================================================
+
+// 15) O‘rtacha qiymat (2 ta son)
+// 2 ta sonning o‘rtacha qiymatini toping
+
+// let num1 = +prompt("enter num1: ");
+// let num2 = +prompt("enter num2: ");
+
+// let res = (num1 + num2) / 2;
+// console.log("O'rta qiymat - ", res);
+// ==============================================================
+
+// 16) O‘rtacha qiymat (3 ta son)
+// 3 ta sonning o‘rtacha qiymatini toping
+
+// let num1 = +prompt("enter num1: ");
+// let num2 = +prompt("enter num2: ");
+// let num3 = +prompt("enter num2: ");
+
+// let res = (num1 + num2 + num3) / 3;
+// console.log("O'rta qiymat - ", res);
+// ============================================================
+
+// 17) Yoshni hisoblash
+// Tug‘ilgan yil orqali yoshni toping.
+
+// let num1 = +prompt("enter year of birth: ");
+
+// console.log("Your age - ", 2026 - num1);
+// ==========================================================
+
+// 18) Soatni minutga aylantirish
+// Soat berilgan. Minutga aylantiring.
+
+// let hours = +prompt("Enter hours:");
+// let res = 60 * hours;
+
+// console.log(res, "minutes");
+// ======================================================
+
+// 19) Minutni sekundga aylantirish
+// Minut berilgan. Sekundga aylantiring.
+
+// let minutes = +prompt("Enter minutes:");
+// let res = 60 * minutes;
+
+// console.log(res, " seconds");
+// =================================================
+
+// 20) Pulni bo‘lish
+// Pul summasi va odamlar soni berilgan. Har biriga qancha tushishini toping.
+
+// let money = +prompt("Enter amount of money");
+// let people = +prompt("Enter amount of people");
+
+// let res = money / people;
+// console.log(res);
