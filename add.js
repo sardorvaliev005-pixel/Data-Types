1c617d46de781733702bd5cbbb5c45e6c8f6365f
Sardor Valiev <sardorvaliev005@gmail.com>
2026-02-22 14:34:33 +0500

    Update add.js

--- add.js
+++ add.js
@@ -1,34 +1,28 @@
-// ======================================================================
-
 // 1) Ikki sonni qo‘shish
-// Foydalanuvchidan 2 ta son olib, yig‘indisini chiqaring
+// 2 ta son kiriting va yig‘indisini chiqaring.
 
-// console.log(name, surname, 'age - ', age, ' address - ',address);
-
-// ===================================================================================
-
-// let num1 = +prompt("Enter num1");
-// let num2 = +prompt("Enter num2");
+// let num1 = +prompt('num1ni kiriting');
+// let num2 = +prompt('num2ni kiriting');
 
 // let res = num1 + num2;
-// console.log("Natija - ", res);
+// console.log(res);
 // ===================================================================================
 
 // 2) Ikki sonni ayirish
-// Birinchi sondan ikkinchi sonni ayiring va natijani chiqaring.
+// Birinchi sondan ikkinchi sonni ayiring.
 
-// let num1 = +prompt("enter num1: ");
-// let num2 = +prompt("enter num2: ");
+// let num1 = +prompt('num1ni kiriting');
+// let num2 = +prompt('num2ni kiriting');
 
 // let res = num1 - num2;
 // console.log(res);
 // =====================================================================
 
 // 3) Ko‘paytirish
-// 2 ta sonni ko‘paytiring va natijani chiqaring.
+// 2 ta sonni ko‘paytiring.
 
-// let num1 = +prompt("enter num1: ");
-// let num2 = +prompt("enter num2: ");
+// let num1 = +prompt('num1ni kiriting');
+// let num2 = +prompt('num2ni kiriting');
 
 // let res = num1 * num2;
 // console.log(res);
@@ -38,8 +32,8 @@
 // 4) Bo‘lish
 // Bir sonni ikkinchi songa bo‘ling
 
-// let num1 = +prompt("enter num1: ");
-// let num2 = +prompt("enter num2: ");
+// let num1 = +prompt('num1ni kiriting');
+// let num2 = +prompt('num2ni kiriting');
 
 // let res = num1 / num2;
 // console.log(res);
@@ -47,10 +41,10 @@
 // =============================================================
 
 // 5) To‘rt amal birga
-// 2 ta son uchun qo‘shish, ayirish, ko‘paytirish va bo‘lishni bajaring.
+// 2 ta son uchun barcha arifmetik amallarni bajaring.
 
-// let num1 = +prompt("enter num1: ");
-// let num2 = +prompt("enter num2: ");
+// let num1 = +prompt('num1ni kiriting');
+// let num2 = +prompt('num2ni kiriting');
 
 // let res1 = num1 + num2;
 // let res2 = num1 - num2;
@@ -67,39 +61,39 @@
 // 6) Kvadratini topish
 // Bitta sonning kvadratini toping.
 
-// let num1 = +prompt("enter num: ");
+// let num1 = +prompt('num1ni kiriting');
 
 // let res = num1 * num1;
-// console.log(num1, ` => `, res);
+// console.log( res);
 
 // ============================================================
 
 // 7) Kubini topish
 // Bitta sonning kubini toping.
 
-// let num1 = +prompt("enter num: ");
+// let num1 = +prompt('num1ni kiriting');
 
-// let res = num1 ** num1;
-// console.log(num1, ` => `, res);
+// let res = num1 * num1;
+// console.log( res);
 
 // ==========================================================
 
 // 8) Kvadrat perimetri
 // Kvadrat tomoni berilgan. Perimetrini toping.
 
-// let num1 = +prompt("enter side: ");
+// let num1 = +prompt('num1ni kiriting');
 
 // let res = 4 * num1;
-// console.log("P - ", res);
+// console.log( res);
 // ==========================================================
 
 // 9) Kvadrat yuzasi
 // Kvadrat yuzasini toping. Formula: S = a * a
 
-// let num1 = +prompt("enter side: ");
+// let num1 = +prompt('num1ni kiriting');
 
 // let res = num1 * num1;
-// console.log("S - ", res);
+// console.log( res);
 // ============================================================
 
 // 11) To‘g‘ri to‘rtburchak yuzasi
@@ -109,7 +103,7 @@
 // let num2 = +prompt("enter side2: ");
 
 // let res = num1 * num2;
-// console.log("S - ", res);
+// console.log( res);
 // ============================================================
 
 // 13) Vaqtni topish
@@ -119,7 +113,7 @@
 // let num2 = +prompt("enter speed: ");
 
 // let res = num1 / num2;
-// console.log("t - ", res);
+// console.log( res);
 // ============================================================
 
 // 14) Masofani topish
@@ -129,7 +123,7 @@
 // let num2 = +prompt("enter time: ");
 
 // let res = num1 * num2;
-// console.log("S - ", res);
+// console.log( res);
 // ============================================================
 
 // 15) O‘rtacha qiymat (2 ta son)
@@ -139,7 +133,7 @@
 // let num2 = +prompt("enter num2: ");
 
 // let res = (num1 + num2) / 2;
-// console.log("O'rta qiymat - ", res);
+// console.log(res);
 // ==============================================================
 
 // 16) O‘rtacha qiymat (3 ta son)
@@ -150,7 +144,7 @@
 // let num3 = +prompt("enter num2: ");
 
 // let res = (num1 + num2 + num3) / 3;
-// console.log("O'rta qiymat - ", res);
+// console.log(res);
 // ============================================================
 
 // 17) Yoshni hisoblash
@@ -182,8 +176,10 @@
 // 20) Pulni bo‘lish
 // Pul summasi va odamlar soni berilgan. Har biriga qancha tushishini toping.
 
-// let money = +prompt("Enter amount of money");
-// let people = +prompt("Enter amount of people");
+// let money = +prompt("Enter money");
+// let people = +prompt("Enter people");
 
 // let res = money / people;
 // console.log(res);
+
+
